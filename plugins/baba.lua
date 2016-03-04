677f867986e8b0ba111569dc7ef73f1659d7c2b6
AMIRUJK <UJKGMT62@GMAIL.COM>
2016-03-04 11:36:40 +0330

    Create baba.lua

---
+++ plugins/baba.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return "با باباییم چیکار داری ؟؟ 🙂🙃"
+  end
+return {
+  description = "botfather", 
+  usage = "admin name: ",
+  patterns = {
+    "^امیر,
+  },
+  run = run
+}
+end
+— by mersad —
